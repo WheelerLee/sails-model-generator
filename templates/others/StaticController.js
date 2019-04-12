4ddc27405bca8519a75017f7baf7e4a5b9e35789
WheelerLee <490386516@qq.com>
2019-04-12 14:51:25 +0800

    文件上传修改

--- templates/others/StaticController.js
+++ templates/others/StaticController.js
@@ -4,16 +4,20 @@
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
-var contentDisposition = require('content-disposition');
-var fs = require('fs');
+const contentDisposition = require('content-disposition');
+const fs = require('fs-extra');
 
 module.exports = {
 
   /**
-   * 下载原始图片
-   * @param req
-   * @param res
-   * @returns {Promise.<*>}
+   * @api {get} /static/:id 下载文件
+   * @apiGroup StaticController
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription 下载文件，如果是图片，则会下载原始图片
+   *
+   * @apiParam {int} id 资源id
+   *
    */
   download: async function (req, res) {
 
@@ -24,24 +28,28 @@ module.exports = {
 
     if (!ath) return res.notFound();
 
-    if (ath.save_path.startsWith('/pdf')) return res.notFound();
-
     var file_path = process.cwd() + '/upload' + ath.save_path + '/' + ath.save_name;
     if (!fs.existsSync(file_path)) {
       return res.notFound();
     }
 
-    // res.set({
-    //   'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
-    //   'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
-    // });
-    // fs.createReadStream(file_path).pipe(res);
-    res.download(file_path, ath.real_name);
+    res.set({
+      'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
+      'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
+    });
+    fs.createReadStream(file_path).pipe(res);
 
   },
 
   /**
-   * 获取大图，如果不存在，则返回原图
+   * @api {get} /static/large/:id 下载文件
+   * @apiGroup StaticController
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription 下载文件，如果是图片，则下载最大1024*1024的压缩图片
+   *
+   * @apiParam {int} id 资源id
+   *
    */
   download_large: async function (req, res) {
 
@@ -52,26 +60,29 @@ module.exports = {
 
     if (!ath) return res.notFound();
 
-    if (ath.save_path.startsWith('/pdf')) return res.notFound();
-
     var file_path = process.cwd() + '/upload' + ath.save_path + '/' + (ath.large_name ? ath.large_name : ath.save_name);
     if (!fs.existsSync(file_path)) {
       return res.notFound();
     }
 
-    // res.set({
-    //   'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
-    //   'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
-    // });
+    res.set({
+      'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
+      'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
+    });
 
-    // fs.createReadStream(file_path).pipe(res);
-    // res.set('Content-Disposition', contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {}));
-    res.download(file_path, ath.real_name);
+    fs.createReadStream(file_path).pipe(res);
 
   },
 
   /**
-   * 获取中图，如果不存在，则返回原图
+   * @api {get} /static/middle/:id 下载文件
+   * @apiGroup StaticController
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription 下载文件，如果是图片，则下载最大512*512的压缩图片
+   *
+   * @apiParam {int} id 资源id
+   *
    */
   download_middle: async function (req, res) {
 
@@ -82,25 +93,29 @@ module.exports = {
 
     if (!ath) return res.notFound();
 
-    if (ath.save_path.startsWith('/pdf')) return res.notFound();
-
     var file_path = process.cwd() + '/upload' + ath.save_path + '/' + (ath.middle_name ? ath.middle_name : ath.save_name);
     if (!fs.existsSync(file_path)) {
       return res.notFound();
     }
 
-    // res.set({
-    //   'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
-    //   'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
-    // });
+    res.set({
+      'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
+      'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
+    });
 
-    // fs.createReadStream(file_path).pipe(res);
-    res.download(file_path, ath.real_name);
+    fs.createReadStream(file_path).pipe(res);
 
   },
 
   /**
-   * 获取小图，如果不存在，则返回原图
+   * @api {get} /static/small/:id 下载文件
+   * @apiGroup StaticController
+   * @apiVersion 1.0.0
+   *
+   * @apiDescription 下载文件，如果是图片，则下载最大256*256的压缩图片
+   *
+   * @apiParam {int} id 资源id
+   *
    */
   download_small: async function (req, res) {
 
@@ -110,20 +125,17 @@ module.exports = {
 
     if (!ath) return res.notFound();
 
-    if (ath.save_path.startsWith('/pdf')) return res.notFound();
-
     var file_path = process.cwd() + '/upload' + ath.save_path + '/' + (ath.small_name ? ath.small_name : ath.save_name);
     if (!fs.existsSync(file_path)) {
       return res.notFound();
     }
 
-    // res.set({
-    //   'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
-    //   'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
-    // });
+    res.set({
+      'Content-Type': ath.file_type ? ath.file_type : 'application/octet-stream',
+      'Content-Disposition': contentDisposition(ath.real_name, ath.file_type && ath.file_type.startsWith('image') ? {type: 'inline'} : {})
+    });
 
-    // fs.createReadStream(file_path).pipe(res);
-    res.download(file_path, ath.real_name);
+    fs.createReadStream(file_path).pipe(res);
 
   },
 
@@ -145,7 +157,7 @@ module.exports = {
       if (req.param('url')) {
         files = await UploadService.uploadFromRemote(req.param('url'));
       } else {
-        files = await UploadService.uploadAttachNew(req.file('file'));
+        files = await UploadService.upload(req.file('file'));
       }
 
       res.json({
--- templates/services/UploadService.js
+++ templates/services/UploadService.js
@@ -1,77 +1,71 @@
 /**
  * 自动生成的文件上传类，如有需要，请自行修改
  */
-var moment = require('moment');
-var fs = require('fs');
-var uuid = require('uuid');
-var axios = require('axios');
-var contentDisposition = require('content-disposition');
-var gm = require('gm').subClass({imageMagick: true});
+const moment = require('moment');
+const fs = require('fs-extra');
+const uuid = require('uuid');
+const axios = require('axios');
+const contentDisposition = require('content-disposition');
+const gm = require('gm').subClass({imageMagick: true});
+const { URL } = require('url');
 
 module.exports = {
 
   /**
-   * 上传文件
-   * @param req
-   * @returns {Promise}
+   * 压缩图片
+   * @param {int} size 压缩后的最大大小
+   * @param {String} original_path 原始文件路径
+   * @param {String} out_path 输出目录
+   * @param {String} file_name 输出文件名
    */
-  uploadFile: function (req, mid_dir) {
+  resize: function (size, original_path, out_path, file_name) {
     return new Promise(function (resolve, reject) {
-      try {
-        var foldName = moment().utc().utcOffset(480).format('YYYYMMDD');
-        req.upload({
-          maxBytes: parseInt(sails.settings.system_settings.maximum_upload_size) * 1024,
-          dirname: "../../upload/" + mid_dir + foldName
-        }, function whenDone(err, uploadedFiles) {
-          if (err)
-            return reject(err);
-          else {
-            var files = [];
-            for (var i in uploadedFiles) {
-              var file = uploadedFiles[i];
-              if (file) {
-                file.save_path = '/' + mid_dir + foldName;
-                var fd = file.fd;
-                fd = fd.replace(/\\/g, "/");
-                var strStart = (fd.lastIndexOf('/')) + 1;
-                file.save_name = fd.substring(strStart, fd.length);
-                files.push(file);
-              }
-            }
-
-            resolve(files);
-          }
-        });
-      } catch (e) {
-        reject(e);
-      }
-    })
-    .then(UploadService.compression);
+      gm(original_path).resize(size, size, '>').noProfile().write(out_path + '/' + file_name, function (err) {
+        if (err) reject(err);
+        else {
+          resolve(file_name);
+        }
+      });
+    });
   },
 
   /**
-   * 压缩图片
-   * @param files
-   * @returns {Promise.<*>}
+   * 记录文件到数据库
+   * @param {Array} files 需要记录的文件
    */
-  compression: function (files) {
-
-    var promises = [];
+  saveToDB: function (files) {
+    let promises = [];
     for (var i in files) {
-      var file = files[i];
-      if (file.type.startsWith('image')) {
-        var promise = new Promise(function (resolve, reject) {
-          var ext_name = file.save_name.substring(file.save_name.lastIndexOf('.'), file.save_name.length);
+      let file = files[i];
+      let promise = Attach.create({
+        real_name: file.filename,
+        save_path: file.save_path,
+        attach_size: file.size,
+        file_type: file.type,
+        save_name: file.save_name,
+        large_name: file.large_name,
+        middle_name: file.middle_name,
+        small_name: file.small_name
+      }).fetch();
+      promises.push(promise);
+    }
+    return Promise.all(promises);
+  },
+
+  /**
+   * 压缩上传的图片
+   * @param {Array} files 上传的文件列表
+   */
+  compression: function(files) {
+    let promises = [];
+    for (let file of files) {
+      var promise = new Promise(function (resolve, reject) {
+        // var ext_name = file.save_name.substring(file.save_name.lastIndexOf('.'), file.save_name.length);
+        if (file.type.startsWith('image')) {
           Promise.all([
-            UploadService.resize(1024,
-              process.cwd() + '/upload/' + file.save_path + '/' + file.save_name, process.cwd() + '/upload/' + file.save_path,
-              uuid.v4() + ext_name),
-            UploadService.resize(512,
-              process.cwd() + '/upload/' + file.save_path + '/' + file.save_name, process.cwd() + '/upload/' + file.save_path,
-              uuid.v4() + ext_name),
-            UploadService.resize(256,
-              process.cwd() + '/upload/' + file.save_path + '/' + file.save_name, process.cwd() + '/upload/' + file.save_path,
-              uuid.v4() + ext_name)
+            UploadService.resize(1024, file.fd, `${process.cwd()}/upload/${file.save_path}`, uuid.v4()),
+            UploadService.resize(512, file.fd, `${process.cwd()}/upload/${file.save_path}`, uuid.v4()),
+            UploadService.resize(256, file.fd, `${process.cwd()}/upload/${file.save_path}`, uuid.v4())
           ]).then(function (out_paths) {
             file.large_name = out_paths[0];
             file.middle_name = out_paths[1];
@@ -80,197 +74,98 @@ module.exports = {
           }).catch(function (e) {
             reject(e);
           });
-        });
-        promises.push(promise);
-      } else {
-        var promise = new Promise(function (resolve, reject) {
+        } else {
           resolve(file);
-        });
-        promises.push(promise);
-      }
-    }
-    return Promise.all(promises);
-
-    // jimp不支持gif压缩
-    // var promises = [];
-    // for (var i in files) {
-    //   var file = files[i];
-    //   if (file.type.startsWith('image')) {
-    //     var promise = new Promise(function (resolve, reject) {
-    //       Jimp.read(file.fd, function (err, image) {
-    //         if (err) reject(err);
-    //         else {
-    //           var largestSize = image.bitmap.width > image.bitmap.height ? image.bitmap.width : image.bitmap.height;
-    //           var ext_name = file.save_name.substring(file.save_name.lastIndexOf('.'), file.save_name.length);
-    //           var largeSize = largestSize > 1024 ? 1024 : largestSize;
-    //           file.large_name = uuid.v4() + ext_name;
-    //           image.scaleToFit(largeSize, largeSize)
-    //             .write(process.cwd() + '/upload/' + file.save_path + '/' + file.large_name);
-    //
-    //           var middleSize = largestSize > 512 ? 512 : largestSize;
-    //           file.middle_name = uuid.v4() + ext_name;
-    //           image.scaleToFit(middleSize, middleSize)
-    //             .write(process.cwd() + '/upload/' + file.save_path + '/' + file.middle_name);
-    //
-    //           var smallSize = largestSize > 256 ? 256 : largestSize;
-    //           file.small_name = uuid.v4() + ext_name;
-    //           image.scaleToFit(smallSize, smallSize)
-    //             .write(process.cwd() + '/upload/' + file.save_path + '/' + file.small_name);
-    //           resolve(file);
-    //         }
-    //       });
-    //     });
-    //     promises.push(promise);
-    //   } else {
-    //     var promise = new Promise(function (resolve, reject) {
-    //       resolve(file);
-    //     });
-    //     promises.push(promise);
-    //   }
-    // }
-    // return Promise.all(promises);
-
-  },
-
-  saveToDB: function (files) {
-    var promises = [];
-    for (var i in files) {
-      var file = files[i];
-      var promise = Attach.create({
-        real_name: file.filename,
-        save_name: file.save_name,
-        save_path: file.save_path,
-        attach_size: file.size,
-        file_type: file.type,
-        large_name: file.large_name,
-        middle_name: file.middle_name,
-        small_name: file.small_name
-      }).fetch();
+        }
+      });
       promises.push(promise);
     }
-
     return Promise.all(promises);
-
   },
 
-  uploadFromRemote: function (url, mid_dir) {
-
-    if (!mid_dir) {
-      mid_dir = 'image/';
-    }
-
-    return new Promise(function (resolve, reject) {
-
-      axios({
-        method: 'get',
-        url: url,
-        responseType: 'stream',
-        timeout: 100000
-      }).then(function (response) {
-
-        if (response.status === 200) {
-
-          var content_type = response.headers['content-type'];
-          var size = response.headers['content-length'];
-
-          var file_name = '';
-          if (response.headers['content-disposition']) {
-            file_name = contentDisposition.parse(response.headers['content-disposition']).parameters.filename;
+  /**
+   * 上传文件
+   * @param {Stream} upStream 上传的文件流
+   */
+  upload: function(upStream) {
+    return (new Promise(function(resolve, reject) {
+      let foldName = moment().utc().utcOffset(480).format('YYYYMMDD');
+      upStream.upload({
+        maxBytes: parseInt(sails.settings.system_settings.maximum_upload_size) * 1024,
+        dirname: "../../upload/" + foldName,
+        saveAs: function (__newFileStream, next) {
+          return next(undefined, uuid.v4());
+        }
+      }, function whenDone(err, files) {
+        if (err) reject(err);
+        else {
+          for (let file of files) {
+            file.save_path = '/' + foldName;
+            let strStart = (file.fd.lastIndexOf('/')) + 1;
+            file.save_name = file.fd.substring(strStart, file.fd.length);
           }
-          if (response.headers['Content-Disposition']) {
-            file_name = contentDisposition.parse(response.headers['Content-Disposition']).parameters.filename;
-          }
-          if (!file_name) {
-            content_type = 'application/octet-stream';
-          }
+          resolve(files);
+        }
+      });
+    })).then(UploadService.compression).then(UploadService.saveToDB);
+  },
 
+  /**
+   * 远程下载文件，如果是图片会进行压缩
+   * @param {String} url 文件链接
+   */
+  uploadFromRemote: function(url) {
+    return axios({
+      method: 'get',
+      url: url,
+      responseType: 'stream',
+      timeout: 100000
+    }).then(function (response) {
+      if (response.status === 200) {
+        
+        var content_type = response.headers['content-type'];
+        var size = response.headers['content-length'];
 
-          var foldName = moment().utc().utcOffset(480).format('YYYYMMDD');
-          var absolute_path = '/' + mid_dir + foldName;
-          var path = process.cwd() + "/upload/" + mid_dir;
-          if (!fs.existsSync(path)) {
-            fs.mkdirSync(path);
+        if (!content_type) {
+          content_type = 'application/octet-stream';
+        }
+        let file_name = '';
+        if (response.headers['content-disposition']) {
+          file_name = contentDisposition.parse(response.headers['content-disposition']).parameters.filename;
+        } else if (response.headers['Content-Disposition']) {
+          file_name = contentDisposition.parse(response.headers['Content-Disposition']).parameters.filename;
+        } else {
+          let link =  new URL(url);
+          let pathnames = link.pathname.split('/');
+          if (pathnames.length > 0) {
+            file_name = pathnames[pathnames.length - 1];
+          } else {
+            file_name = uuid.v4();
           }
-          path += foldName + '/';
-          if (!fs.existsSync(path)) {
-            fs.mkdirSync(path);
-          }
+        }
+        let foldName = moment().utc().utcOffset(480).format('YYYYMMDD');
+        let path = process.cwd() + "/upload/" + foldName + '/';
+        fs.ensureDirSync(path);
 
-          var ext_name = file_name.substring(file_name.lastIndexOf('.'), file_name.length);
-          var save_name = uuid.v4() + ext_name;
-
-          var stream = response.data.pipe(fs.createWriteStream(path + save_name));
-          stream.on('finish', function () {
+        let save_name = uuid.v4();
+        return new Promise(function(resolve, reject) {
+          let stream = response.data.pipe(fs.createWriteStream(path + save_name));
+          stream.on('finish', function() {
             resolve([{
-              filename: file_name ? file_name : save_name,
+              filename: file_name,
               save_name: save_name,
-              save_path: absolute_path,
+              save_path: '/' + foldName,
               size: size ? size : 0,
               type: content_type,
               fd: path + save_name
             }]);
-
           });
+        });
 
-        } else {
-          reject(new Error());
-        }
-
-      }).catch(function (e) {
-        reject(e);
-      });
-
+      } else {
+        return new Error();
+      }
     }).then(UploadService.compression).then(UploadService.saveToDB);
-
-  },
-
-  resize: function (size, original_path, out_path, out_file) {
-    return new Promise(function (resolve, reject) {
-      gm(original_path).resize(size, size, '>').noProfile().write(out_path + '/' + out_file, function (err) {
-        if (err) reject(err);
-        else {
-          resolve(out_file);
-        }
-      });
-      // gm(original_path).thumb(size, size, out_path + '/' + out_file, 100, function (err, stdout, stderr, command) {
-      //   if (err) reject(err);
-      //   else {
-      //     resolve(out_file);
-      //   }
-      // });
-    });
-  },
-
-  /**
-   * 上传附件，文件将上传包保存到附件表
-   * @param req 上传的http请求
-   * @param user_id 上传的用户id
-   * @returns {Promise<U>|Thenable<U>|*}
-   * @deprecated
-   */
-  uploadAttach: function (req, mid_dir) {
-
-    if (!mid_dir) {
-      mid_dir = 'image/';
-    }
-
-    return this.uploadAttachNew(req.file('avatar'), user_id, mid_dir);
-
-  },
-
-  /**
-   * 上传附件，文件将上传包保存到附件表
-   * @param upStream 上传的http请求的form字段
-   * @param user_id 上传的用户id
-   * @returns {Promise<U>|Thenable<U>|*}
-   */
-  uploadAttachNew: function (upStream, mid_dir) {
-
-    if (!mid_dir) {
-      mid_dir = 'image/';
-    }
-    return this.uploadFile(upStream, mid_dir).then(UploadService.saveToDB);
-
   }
 
 };
