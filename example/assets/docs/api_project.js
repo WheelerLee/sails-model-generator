1459320fbee10acf61f0d14aebd7c16506ee30a9
wheeler <490386516@qq.com>
2020-07-30 14:06:41 +0800

    文档

---
+++ example/assets/docs/api_project.js
@@ -0,0 +1,21 @@
+define({
+  "name": "代码生成器",
+  "version": "1.0.0",
+  "description": "自动生成的接口",
+  "title": "代码生成器",
+  "url": "http://192.168.8.254:1337",
+  "sampleUrl": "http://192.168.8.254:1337",
+  "template": {
+    "withCompare": true,
+    "forceLanguage": "zh_cn",
+    "withGenerator": false
+  },
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2020-07-30T05:38:29.139Z",
+    "url": "http://apidocjs.com",
+    "version": "0.22.1"
+  }
+});
